33fe0cdc4f2fcdd8abd45dac23603a9dd0b31c98
Alexandr <shevajoker@gmail.com>
2025-01-26 20:23:32 +0300

    [119] - added DashboardLineChart

--- src/main/java/com/pdrw/pdrw/dashboard/entity/linechart/DashboardLineChart.java
+++ src/main/java/com/pdrw/pdrw/dashboard/entity/linechart/DashboardLineChart.java
@@ -1,6 +1,15 @@
 package com.pdrw.pdrw.dashboard.entity.linechart;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class DashboardLineChart {
+
+    List<String> labels = new ArrayList<>();
+    List<Dataset> datasets = new ArrayList<>();
 
 
 }
